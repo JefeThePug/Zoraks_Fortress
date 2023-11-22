5485a8935cc0433ffd22b5888412b3357ca975d2
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2023-11-23 00:34:15 +0900

    Added session and a tag for future js scripts

--- zoraks_fortress/zorakapp/__init__.py
+++ zoraks_fortress/zorakapp/__init__.py
@@ -1,11 +1,10 @@
 from flask import Flask
-#from flask_sqlalchemy import SQLAlchemy
-
 
 
 app = Flask(__name__)
+app.secret_key = 'abcdefg1234567'
 #?? app.config.from_pyfile('config.py', silent=True)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////app.db'
-#db = SQLAlchemy(app)
+#?? app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////app.db'
+#?? db = SQLAlchemy(app)
 
 from zorakapp import views
--- zoraks_fortress/zorakapp/views.py
+++ zoraks_fortress/zorakapp/views.py
@@ -1,5 +1,24 @@
+from flask import render_template, request, make_response, session
 from zorakapp import app
+import sys
+import uuid
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        user_id = str(uuid.uuid4())
+        session['user_id'] = user_id
+
+        response = make_response(render_template('game.html'))
+        response.set_cookie('user_id', user_id)
+
+        print('new! User ID:', user_id, file=sys.stderr)
+        return response
+
+    print('User ID:', user_id, file=sys.stderr)
+    return render_template('game.html')
+
+
+
