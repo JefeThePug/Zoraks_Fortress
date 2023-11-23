d16d68cd14cea9fd8f51852cf496e81f3b3739e1
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2023-11-23 12:38:36 +0900

    starting text, flask session, bg image edit

--- zoraks_fortress/zorakapp/views.py
+++ zoraks_fortress/zorakapp/views.py
@@ -20,5 +20,19 @@ def index():
     print('User ID:', user_id, file=sys.stderr)
     return render_template('game.html')
 
+@app.route('/map')
+def map():
+    user_id = session.get('user_id')
 
+    if user_id is None:
+        user_id = str(uuid.uuid4())
+        session['user_id'] = user_id
 
+        response = make_response(render_template('map.html'))
+        response.set_cookie('user_id', user_id)
+
+        print('new! User ID:', user_id, file=sys.stderr)
+        return response
+
+    print('User ID:', user_id, file=sys.stderr)
+    return render_template('map.html')
